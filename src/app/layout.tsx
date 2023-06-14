331aadcf5fcd8ae5ea8c3f7afa03db1b89fc2eb6
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-14 20:10:51 -0300

    Add MantineProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import React from 'react';
-import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 const inter = Inter({ subsets: ['latin'] })
 
 
@@ -14,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>   <MantineProvider>{children}</MantineProvider></body>
+      <body className={inter.className}>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   )
 }
